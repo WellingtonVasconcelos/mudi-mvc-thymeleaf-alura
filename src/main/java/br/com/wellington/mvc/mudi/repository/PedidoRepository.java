6c44fbd1e13d2b7a763f08cde780b0a0ad76f5e2
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-12-08 17:02:02 -0300

    Routes status

--- src/main/java/br/com/wellington/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/wellington/mvc/mudi/repository/PedidoRepository.java
@@ -1,12 +1,17 @@
 package br.com.wellington.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.wellington.mvc.mudi.model.Pedido;
+import br.com.wellington.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	List<Pedido> findByStatus(StatusPedido status);
 	
 
 }
