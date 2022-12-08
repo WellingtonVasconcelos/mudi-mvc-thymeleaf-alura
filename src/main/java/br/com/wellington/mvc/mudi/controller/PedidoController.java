b2f970767f6855efda241e2d882a62f9bb20f38d
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-12-08 12:46:07 -0300

    Finishing top | creating template

--- src/main/java/br/com/wellington/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/wellington/mvc/mudi/controller/PedidoController.java
@@ -1,16 +1,40 @@
 package br.com.wellington.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.wellington.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.wellington.mvc.mudi.model.Pedido;
+import br.com.wellington.mvc.mudi.repository.PedidoRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 	
-	@GetMapping("formulario")
-	public String formulario() {
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@GetMapping("formulario") 
+	public String formulario(RequisicaoNovoPedido requisicao) {
 		return "pedido/formulario";
 	}
-
+	
+	@PostMapping("novo")
+	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		
+		return "pedido/formulario";
+	}
+	
 }
